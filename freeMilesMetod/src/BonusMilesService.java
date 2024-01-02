a94bd80b7426a1a4a14cec2df1368da6b2f7e15f
natali.r <natasha-razdobreeva@mail.ru>
2024-01-03 00:36:39 +0300

    free miles metod kod

--- freeMilesMetod/src/BonusMilesService.java
+++ freeMilesMetod/src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
-    public int calculate (int price) {
+    public int calculate(int price) {
         int result;
-        result = price/20;
+        result = price / 20;
         return result;
     }
 
